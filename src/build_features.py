1f8f8f860ebca9f496bf1f516260f1a2a9f066e6
Danius Silkaitis <danius.silkaitis@gmail.com>
2016-12-20 13:37:04 -0800

    Added two feature functions

--- src/build_features.py
+++ src/build_features.py
@@ -13,14 +13,28 @@ def drop_missing(df):
     '''
     return(df.where(df.HomeTeam.isNotNull()))
 
+def location_label(location):
+    '''
+    Create dictionary label
+    '''
+    if location == 'Home':
+        return('HomeTeam_Id')
+    else:
+        return('AwayTeam_Id')
+
+def switch_label(location):
+    '''
+    Switch label
+    '''
+    d = {'Home': 'Away', 'Away': 'Home'}
+
+    return(d[location])
+
 def team_metric_location(team_id, metric='YellowCards', location='Home', rdd):
     '''
     Calculate metric average for team given home or away
     '''
-    if location == 'Home':
-        lbl = 'HomeTeam_Id'
-    else:
-        lbl = 'AwayTeam_Id'
+    lbl = location_label(location)
 
     val = rdd.filter(lambda row: row[lbl] == team_id) \
              .map(lambda row: (int(row[location + metric]), 1)) \
@@ -28,11 +42,20 @@ def team_metric_location(team_id, metric='YellowCards', location='Home', rdd):
 
     return(val[0] / float(val[1]))
 
-def team_metric_opponent(team_id, opp_id, rdd):
+def team_metric_opponent(team_id, opp_id, metric='YellowCards', team_loc='Home', rdd):
     '''
-    Calculate average number of yellow cards given an opponent
+    Calculate metric average for team given opponent and location
     '''
-    pass
+    opp_loc = switch_label(team_loc)
+
+    lbl_t = location_label(team_loc)
+    lbl_o = location_label(opp_loc)
+
+    val = rdd.filter(lambda row: (row[lbl_t] == team_id) & (row[lbl_o] == opp_id)) \
+             .map(lambda row: (int(row[team_loc + metric]), 1)) \
+             .reduce(lambda c, p: (c[0] + p[0], c[1] + p[1]))
+
+    return(val[0] / float(val[1]))
 
 if __name__ == '__main__':
     #Do stuff
