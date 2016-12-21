8cde59aa410344b19b6c6b5d574af6ee0bd8df8d
Danius Silkaitis <danius.silkaitis@gmail.com>
2016-12-21 09:51:33 -0800

    Added data cleaning

--- src/build_features.py
+++ src/build_features.py
@@ -1,17 +1,46 @@
 import pyspark as pys
 
+from pyspark.sql.functions import udf
+
 def load_s3_json(ftype, bucket, session):
     '''
     Extract file names stored in s3 bucket and return RDD
     '''
     data = session.read.json(bucket + '*.' + ftype)
-    return(data.rdd)
+    return(data)
 
 def drop_missing(df):
     '''
     Remove completely blank rows from DataFrame
     '''
     return(df.where(df.HomeTeam.isNotNull()))
+
+def convert_data_type(data, label):
+    '''
+    Convert strings to int
+    '''
+    return(data.withColumn(label, data[label].cast('int')))
+
+def convert_columns(data):
+    '''
+    Iterate through columns and convert
+    '''
+    cnvrt = set(['Corners',
+                 'Fouls',
+                 'Goals',
+                 'Cards',
+                 'Shots',
+                 'ShotsOnTarget',
+                 'HalfTimeGoals',
+                 'Team_Id'])
+
+    for col in data.columns:
+        col_reduced = col.replace('Away','').replace('Home','')
+
+        if col_reduced in cnvrt:
+            data = convert_data_type(data, col)
+
+    return(data)
 
 def location_label(location):
     '''
@@ -30,19 +59,20 @@ def switch_label(location):
 
     return(d[location])
 
-def team_metric_location(team_id, metric='YellowCards', location='Home', rdd):
+def team_metric_location(rdd, metric='YellowCards', location='Home'):
     '''
-    Calculate metric average for team given home or away
+    Calculate metric average for teams given home or away
     '''
     lbl = location_label(location)
 
-    val = rdd.filter(lambda row: row[lbl] == team_id) \
-             .map(lambda row: (int(row[location + metric]), 1)) \
-             .reduce(lambda c, p: (c[0] + p[0], c[1] + p[1]))
+    val = rdd.map(lambda row: (row[lbl],(row[location + metric], 1))) \
+             .reduceByKey(lambda (x, y): (x[0] + y[0], x[1] + y[1])) \
+             .map(lambda (t, (m, g)): (t, f / float(g))) \
+             .collectAsMap()
 
-    return(val[0] / float(val[1]))
+    return(val)
 
-def team_metric_opponent(team_id, opp_id, metric='YellowCards', team_loc='Home', rdd):
+def team_metric_opponent(team_id, opp_id, rdd, metric='YellowCards', team_loc='Home'):
     '''
     Calculate metric average for team given opponent and location
     '''
@@ -52,7 +82,7 @@ def team_metric_opponent(team_id, opp_id, metric='YellowCards', team_loc='Home',
     lbl_o = location_label(opp_loc)
 
     val = rdd.filter(lambda row: (row[lbl_t] == team_id) & (row[lbl_o] == opp_id)) \
-             .map(lambda row: (int(row[team_loc + metric]), 1)) \
+             .map(lambda row: (row[team_loc + metric], 1)) \
              .reduce(lambda c, p: (c[0] + p[0], c[1] + p[1]))
 
     return(val[0] / float(val[1]))
@@ -62,13 +92,38 @@ def extract_base_data(rdd):
     Extract data needed to create analytical base table
     (match_id, HomeTeam_Id, AwayTeam_Id, HomeYellowCards, AwayYellowCards)
     '''
-    val = rdd.map(lambda row: (int(row['match_id']),
-                              (int(row['HomeTeam_Id']),
-                               int(row['AwayTeam_Id']),
-                               int(row['HomeYellowCards']),
-                               int(row['AwayYellowCards']))))
+    val = rdd.map(lambda row: (row['match_id'],
+                              (row['HomeTeam_Id'],
+                               row['AwayTeam_Id'],
+                               row['HomeYellowCards'],
+                               row['AwayYellowCards'])))
+
+    return(val.collect())
+
+def team_list(rdd):
+    '''
+    Collect team ids
+    '''
+    val = rdd.map(lambda row: row['HomeTeam_Id']) \
+             .distinct() \
+             .collect()
 
     return(val)
 
 if __name__ == '__main__':
-    #Do stuff
+    #Initial Spark session
+    sc = pys.SparkContext()
+    session = pys.sql.SparkSession(sc)
+
+    #Load data and prep
+    data = load_s3_json('json', 's3://d-sparkbucket/fixtures/', session)
+    data = drop_missing(data)
+    data = convert_columns(data)
+
+    #Switch DataFrame to RDD
+    rdd = data.rdd
+
+    #Collect team metric given location
+    h_yellow = team_metric_location(rdd, metric='YellowCards', location='Home')
+
+    print(h_yellow)
